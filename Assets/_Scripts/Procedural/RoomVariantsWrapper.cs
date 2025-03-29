d4719400bbb4de5a5be595d4162f4cb3bc1c3314
TutusBOT <barte77pl@gmail.com>
2025-03-29 14:13:39 +0100

    feat: spawn rooms with room generator

--- Assets/_Scripts/Procedural/RoomVariantsWrapper.cs
+++ Assets/_Scripts/Procedural/RoomVariantsWrapper.cs
@@ -3,42 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class RoomVariantsEntry {
-  public DungeonGenerator.RoomSize size;
-  public List<GameObject> variants;
+public class RoomVariantsEntry
+{
+    public DungeonGenerator.RoomSize size;
+
+    public RoomVariantData variantData = new RoomVariantData();
 }
 
 [Serializable]
-public class RoomVariantData {
-  public List<GameObject> normalVariants;
+public class RoomVariantData
+{
+    public List<GameObject> normalVariants;
+    public bool allowDoorsAnywhere = true;
+    public List<Vector2Int> allowedNorthDoors;
+    public List<Vector2Int> allowedSouthDoors;
+    public List<Vector2Int> allowedEastDoors;
+    public List<Vector2Int> allowedWestDoors;
 }
 
 [Serializable]
-public class RoomVariantsWrapper {
-  public List<RoomVariantsEntry> roomVariantsList = new List<RoomVariantsEntry>();
+public class RoomVariantsWrapper
+{
+    public List<RoomVariantsEntry> roomVariantsList = new List<RoomVariantsEntry>();
 
-  public Dictionary<DungeonGenerator.RoomSize, RoomVariantData> ToDictionary() {
-    Dictionary<DungeonGenerator.RoomSize, RoomVariantData> dictionary =
-        new Dictionary<DungeonGenerator.RoomSize, RoomVariantData>();
+    public Dictionary<DungeonGenerator.RoomSize, RoomVariantData> ToDictionary()
+    {
+        Dictionary<DungeonGenerator.RoomSize, RoomVariantData> dictionary =
+            new Dictionary<DungeonGenerator.RoomSize, RoomVariantData>();
 
-    foreach (RoomVariantsEntry entry in roomVariantsList) {
-      RoomVariantData data = new RoomVariantData {
-        normalVariants = entry.variants
-      };
-      dictionary[entry.size] = data;
+        foreach (RoomVariantsEntry entry in roomVariantsList)
+        {
+            dictionary[entry.size] = entry.variantData;
+        }
+
+        return dictionary;
     }
 
-    return dictionary;
-  }
-
-  public void FromDictionary(Dictionary<DungeonGenerator.RoomSize, RoomVariantData> dictionary) {
-    roomVariantsList = new List<RoomVariantsEntry>();
-    foreach (var pair in dictionary) {
-      RoomVariantsEntry entry = new RoomVariantsEntry {
-        size = pair.Key,
-        variants = pair.Value.normalVariants
-      };
-      roomVariantsList.Add(entry);
+    public void FromDictionary(Dictionary<DungeonGenerator.RoomSize, RoomVariantData> dictionary)
+    {
+        roomVariantsList = new List<RoomVariantsEntry>();
+        foreach (var pair in dictionary)
+        {
+            RoomVariantsEntry entry = new RoomVariantsEntry
+            {
+                size = pair.Key,
+                variantData = pair.Value,
+            };
+            roomVariantsList.Add(entry);
+        }
     }
-  }
 }
